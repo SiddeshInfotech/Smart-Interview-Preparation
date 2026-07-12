474f0880318f836c59b74fb11e8cbe6c11924d9b
Kimaya Patil <kimayapatil0801@gmail.com>
2026-07-12 18:19:47 +0530

    installed and setup gemini ai

--- backend/resume/models.py
+++ backend/resume/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class ResumeAnalysis(models.Model):
+    analysis_id = models.AutoField(primary_key=True)
+
+    resume = models.OneToOneField(
+        Resume,
+        on_delete=models.CASCADE,
+        db_column="resume_id"
+    )
+
+    extracted_skills = models.TextField(blank=True, null=True)
+    resume_score = models.IntegerField(blank=True, null=True)
+    summary = models.TextField(blank=True, null=True)
+    suggestions = models.TextField(blank=True, null=True)
+
+    analyzed_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = "Resume_Analysis"
+
+    def __str__(self):
+        return f"Analysis {self.analysis_id}"
