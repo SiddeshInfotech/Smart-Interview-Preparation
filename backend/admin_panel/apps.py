52dd7c03e4bea4a0ba929eeb256fab988ee83c3e
aditya903473 <adityabagul312@gmail.com>
2026-07-23 22:03:46 +0530

    fix: correct AppConfig name to admin_panel in admin_panel/apps.py

--- backend/admin_panel/apps.py
+++ backend/admin_panel/apps.py
@@ -1,5 +1,7 @@
 from django.apps import AppConfig
 
 
-class AdminConfig(AppConfig):
-    name = 'admin'
+class AdminPanelConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'admin_panel'
+
